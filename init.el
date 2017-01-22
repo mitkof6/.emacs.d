7ff627ad090aa31b84542715b696a088a12ee292
Dimitris Stanev <jimstanev@gmail.com>
2017-01-22 15:37:04 +0000

    some cleanup

--- init.el
+++ init.el
@@ -7,25 +7,13 @@
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
-(defconst *spell-check-support-enabled* t) ;; Enable with t if you prefer
-(defconst *is-a-mac* (eq system-type 'darwin))
+;;(setq warning-minimum-level :emergency) ;; disable warning message
 
 ;;----------------------------------------------------------------------------
 ;; Bootstrap config
 ;;----------------------------------------------------------------------------
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-;; (require 'init-compat)
 (require 'init-utils)
-
-;; Needed for Emacs version < 24. must come before elpa, as it may provide package.el
-;; (require 'init-site-lisp)
-
-;; Security configuration.
-;; This is commented out by default, but for security considerations
-;; I strongly recommend you to uncomment it.
-;; You may need `gnutls' library and the `certifi' python package to enable this.
-;; see the comment in `init-security.el'
-;; (require 'init-security)
 
 ;; Machinery for installing required packages.
 ;; explicitly call 'package-initialize to set up all packages installed via ELPA.
@@ -37,99 +25,30 @@
 ;; Load configs for specific features and modes
 ;;----------------------------------------------------------------------------
 
-;; (require-package 'wgrep)
-;; (require-package 'project-local-variables)
-;; (require-package 'diminish)
-;; (require-package 'scratch)
-;; (require-package 'mwe-log-commands)
-
-;; (require 'init-frame-hooks)
-;; (require 'init-xterm)
-;; (require 'init-osx-keys)
-;; (require 'init-gui-frames)
-;; (require 'init-proxies)
+;; (require 'init-security)
 (require 'init-dired)
-;; (require 'init-isearch)
-;; (require 'init-uniquify)
-;; (require 'init-ibuffer)
 ;; (require 'init-flycheck)
-
-;; custom
+;; (require 'init-flyspell)
 (require 'init-c-style)
-;; (require 'init-paredit)
 (require 'init-irony)
-
+;; (require 'init-ispell)
 (require 'init-recentf)
 (require 'init-ido)
 (require 'init-yasnippet)
 (require 'init-hippie-expand)
 (require 'init-auto-complete)
-;; (require 'init-windows)
-;; (require 'init-sessions)
 (require 'init-fonts)
-;; (require 'init-mmm)
 (require 'init-tabbar)
 (require 'init-editing-utils)
 (require 'init-evil)
 (require 'init-matlab)
-
-;; (require 'init-vc)
-;; (require 'init-darcs)
 (require 'init-git)
-
-;; (require 'init-crontab)
-;; (require 'init-textile)
 (require 'init-markdown)
 (require 'init-auctex)
-;; (require 'init-csv)
-;; (require 'init-erlang)
-;; (require 'init-javascript)
-;; (require 'init-php)
 (require 'init-org)
-;; (require 'init-nxml)
-;; (require 'init-html)
-;; (require 'init-css)
-;; (require 'init-haml)
 ;; (require 'init-python-mode)
-;; (require 'init-haskell)
-;; (require 'init-ruby-mode)
-;; (require 'init-rails)
-;; (require 'init-sql)
-
-;; (require 'init-paredit)
-;; (require 'init-lisp)
-;; (require 'init-slime)
-;; (require 'init-clojure)
-;; (when (>= emacs-major-version 24)
-;;   (require 'init-clojure-cider))
-;; (require 'init-common-lisp)
-
-;; (when *spell-check-support-enabled*
-;;   (require 'init-spelling))
-
-;; (require 'init-marmalade)
-;; (require 'init-misc)
-
-;; (require 'init-dash)
-;; (require 'init-ledger)
-;; ;; Extra packages which don't require any configuration
-
-;; (require-package 'gnuplot)
-;; (require-package 'lua-mode)
-;; (require-package 'htmlize)
-;; (require-package 'dsvn)
-;; (when *is-a-mac*
-;;   (require-package 'osx-location))
-;; (require-package 'regex-tool)
 
 (require 'init-themes)
-;; ;;----------------------------------------------------------------------------
-;; ;; Allow access from emacsclient
-;; ;;----------------------------------------------------------------------------
-;; (require 'server)
-;; (unless (server-running-p)
-;;   (server-start))
-
 
 ;;----------------------------------------------------------------------------
 ;; Variables configured via the interactive 'customize' interface
@@ -137,24 +56,11 @@
 (when (file-exists-p custom-file)
   (load custom-file))
 
-
 ;;----------------------------------------------------------------------------
 ;; Allow users to provide an optional "init-local" containing personal settings
 ;;----------------------------------------------------------------------------
 (when (file-exists-p (expand-file-name "init-local.el" user-emacs-directory))
   (error "Please move init-local.el to ~/.emacs.d/lisp"))
 (require 'init-local nil t)
-
-
-;; ;;----------------------------------------------------------------------------
-;; ;; Locales (setting them earlier in this file doesn't work in X)
-;; ;;----------------------------------------------------------------------------
-;; (require 'init-locales)
-
-;; (add-hook 'after-init-hook
-;;            (lambda ()
-;;              (message "init completed in %.2fms"
-;;                       (sanityinc/time-subtract-millis after-init-time before-init-time))))
-
 
 (provide 'init)
