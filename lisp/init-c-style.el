e6aded2fede150f2f07480381c2afc1434972e28
Dimitar Stanev <jimstanev@gmail.com>
2017-01-23 01:54:02 -0800

    correct tabs and window problem

--- lisp/init-c-style.el
+++ lisp/init-c-style.el
@@ -11,7 +11,7 @@
 
  (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60))
  (setq tab-width 4)
- (setq indent-tabs-mode t)  ; use spaces only if nil
+ (setq indent-tabs-mode nil)  ; use spaces only if nil
  )
 
 (add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
