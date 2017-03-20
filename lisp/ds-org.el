9a7568e92a516dd339006766deaed4965079149b
Dimitar Stanev <jimstanev@gmail.com>
2017-03-21 01:01:25 +0200

    Some minor improvements for org-mode.

--- lisp/ds-org.el
+++ lisp/ds-org.el
@@ -1,19 +1,25 @@
 ;; Enable org-mode
 (require-package 'org)
+
 ;; Make org-mode work with files ending in .org
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-;; The above is the default in recent emacs
 
 ;; basic global key combinations are defined
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
+(define-key global-map "\C-cc" 'org-capture)
+(define-key global-map "\C-ci" 'org-iswitchb)
 
 ;; turn flyspell on
 (add-hook 'org-mode-hook 'turn-on-flyspell)
 
-;; add workflow
+;; defines todo sequence (global)
 (setq org-todo-keywords
       '((sequence "TODO" "IN-PROGRESS" "WAITING" "DONE")))
+
+;; add done time stamp
+(setq org-log-done t)
+;; add note when done
+(setq org-log-done 'note)
 
 (provide 'ds-org)
