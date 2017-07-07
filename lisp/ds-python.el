f7884be504c8a246f2bd580428b675dc48a5180b
Dimitar Stanev <jimstanev@gmail.com>
2017-07-07 08:46:13 -0700

    twicks for ein

--- lisp/ds-python.el
+++ lisp/ds-python.el
@@ -1,36 +1,36 @@
 (use-package elpy
-	     :ensure t
-	     :defer t
-	     :config
-	     (require-package 'py-autopep8)
-	     (package-initialize)
-	     (elpy-enable)
-	     (elpy-use-ipython)
+  :ensure t
+  :defer t
+  :config
+  (require-package 'py-autopep8)
+  (package-initialize)
+  (elpy-enable)
+  (elpy-use-ipython)
 
-	     ;; this should be set in due to ipython v5
-	     ;; https://github.com/jorgenschaefer/elpy/issues/949
-	     (setq python-shell-interpreter "ipython2"
-		   python-shell-interpreter-args "--simple-prompt --pprint"
-		   elpy-rpc-python-command "python2")
+  ;; this should be set in due to ipython v5
+  ;; https://github.com/jorgenschaefer/elpy/issues/949
+  (setq python-shell-interpreter "ipython2"
+	python-shell-interpreter-args "--simple-prompt --pprint"
+	elpy-rpc-python-command "python2")
 
-	     (add-hook 'python-mode-hook 'elpy-mode)
-	     (with-eval-after-load 'elpy
-	       (remove-hook 'elpy-modules 'elpy-module-flymake)
-	       (add-hook 'elpy-mode-hook 'flycheck-mode)
-	       (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)))
+  (add-hook 'python-mode-hook 'elpy-mode)
+  (with-eval-after-load 'elpy
+    (remove-hook 'elpy-modules 'elpy-module-flymake)
+    (add-hook 'elpy-mode-hook 'flycheck-mode)
+    (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)))
 
 
 (use-package ein
-	     :ensure t
-	     :defer t
-	     :config
-	     (require-package 'smartrep)
-	     ;; (setq ein:use-auto-complete t)
-	     ;; Or, to enable "superpack" (a little bit hacky improvements):
-	     (setq ein:use-auto-complete-superpack t
+  :ensure t
+  :defer t
+  :config
+  (require-package 'smartrep)
+  (setq
+   ein:use-smartrep t
+   ein:use-auto-complete-superpack t
+   ein:output-type-preference '(emacs-lisp svg png jpeg html text latex javascript))
+  )
 
-		   ein:use-smartrep t
-		   elpy-rpc-backend "jedi"))
 
 ;; ;; jedi
 ;; (require-package 'jedi)
