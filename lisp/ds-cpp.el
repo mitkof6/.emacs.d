31f8286eb809c5a6e5cfa770f2dd5cb114ecb115
Dimitar Stanev <jimstanev@gmail.com>
2017-12-09 22:19:18 +0200

    improve compilation by removing custom code and letting default cmake-ide-compile

--- lisp/ds-cpp.el
+++ lisp/ds-cpp.el
@@ -11,7 +11,14 @@
 (require 'rtags)
 (require 'cl)
 
+;; setup company
 (global-company-mode)
+(setq company-idle-delay nil
+      company-minimum-prefix-length 2
+      company-show-numbers t
+      company-tooltip-limit 20
+      company-dabbrev-downcase nil
+      )
 
 ;; setup rtags
 (setq rtags-completions-enabled t
@@ -20,8 +27,7 @@
 (rtags-diagnostics)
 (rtags-enable-standard-keybindings)
 
-
-;; irony-mode hook that is called when irony is triggered
+;; setup irony
 (add-hook 'c++-mode-hook 'irony-mode)
 (add-hook 'c-mode-hook 'irony-mode)
 (add-hook 'objc-mode-hook 'irony-mode)
@@ -35,44 +41,18 @@
 (add-hook 'irony-mode-hook 'ds/irony-mode-hook)
 (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
 
-;; company-irony setup, c-header completions
 (add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
-;; Remove company-semantic because it has higher precedance than company-clang
-;; Using RTags completion is also faster than semantic, it seems. Semantic
-;; also provides a bunch of technically irrelevant completions sometimes.
-;; All in all, RTags just seems to do a better job.
-;; (setq company-backends (delete 'company-semantic company-backends))
-;; Enable company-irony and several other useful auto-completion modes
-;; We don't use rtags since we've found that for large projects this can cause
-;; async timeouts. company-semantic (after company-clang!) works quite well
-;; but some knowledge some knowledge of when best to trigger is still necessary.
 (eval-after-load 'company
                  '(add-to-list
-                   'company-backends '(;; company-irony-c-headers
-                                       company-irony
-                                       company-yasnippet
-                                       ;; company-clang
+                   'company-backends '(company-irony
                                        company-rtags
+                                       company-yasnippet
+                                       company-irony-c-headers
+                                       company-clang
                                        )))
 
-
-(setq company-idle-delay nil
-      company-minimum-prefix-length 2
-      company-show-numbers t
-      company-tooltip-limit 20
-      company-dabbrev-downcase nil
-
-)
-
-;; Windows performance tweaks
-(when (boundp 'w32-pipe-read-delay)
-  (setq w32-pipe-read-delay 0))
-;; Set the buffer size to 64K on Windows (from the original 4K)
-(when (boundp 'w32-pipe-buffer-size)
-  (setq irony-server-w32-pipe-buffer-size (* 64 1024)))
-
 ;;------------------------------------------------------------------------------
-;; company statistics (sorting of completion candidates by frequency)
+;; company statistics for sorting of completion candidates by frequency
 ;;------------------------------------------------------------------------------
 
 (require-package 'company-statistics)
@@ -96,8 +76,8 @@
   (cond ((and (not (use-region-p))
               (ds/irony-check-expansion))
          ;; (message "complete")
-         ;; (company-irony 'interactive)	; better completion
-         (company-complete-common)
+         (company-irony 'interactive)        ; better completion
+         ;; (company-complete)
          )
         (t
          ;; (message "indent")
@@ -111,9 +91,6 @@
 ;;------------------------------------------------------------------------------
 ;; flycheck-irony
 ;;------------------------------------------------------------------------------
-
-;; for some reason flycheck-irony have problems with .dir-locals.el and
-;; ignores them
 
 (require-package 'flycheck-irony)
 (eval-after-load 'flycheck
@@ -168,52 +145,18 @@
 ;; make sure cmake-mode is installed for viewing CMake files
 (require-package 'cmake-mode)
 
-;;---------------------------------------------------------------------
-;; compiling
-;;---------------------------------------------------------------------
+;;------------------------------------------------------------------------------
+;; https://github.com/tuhdo/semantic-refactor
+;;------------------------------------------------------------------------------
 
-(require-package 'compile)
+;; not working properly yet
 
-;; Change compilation command:
-(setq compile-command "make ")
+(require-package 'srefactor)
 
-;; if there are no errors make the compilation window vanish
-(setq compilation-finish-function
-      (lambda (buf str)
-        (if (null (string-match ".*exited abnormally.*" str))
-            ;;no errors, make the compilation window go away in a few seconds
-            (progn
-              (run-at-time
-               "2 sec" nil 'delete-windows-on
-               (get-buffer-create "*compilation*"))
-              (message "No Compilation Errors!")))))
+(semantic-mode 1)
 
-(defun ds/compile ()
-  "Run compile and resize the compile window"
-  (interactive)
-  (progn
-    (call-interactively 'compile)
-    (setq w (get-buffer-window "*compilation*"))
-    (setq cur (selected-window))
-    (select-window w)
-    (setq h (window-height w))
-    (shrink-window (- h 15))
-    (select-window cur)))
-;; (global-set-key [f9] 'ds/compile)
-(define-key c++-mode-map (kbd "C-c C-c") #'ds/compile)
-
-;; useful but when you jump to error it splits vertically,
-;; thus I disable this feature
-;; when the compile window shows always split vertically
-;; (defadvice compile (around split-horizontally activate)
-;;   (let ((split-width-threshold nil)
-;;         (split-height-threshold 0))
-;;     ad-do-it))
-
-(setq compilation-ask-about-save nil  ; Just save before compiling
-      compilation-always-kill t       ; Just kill old compile processes before
-      compilation-scroll-output 'first-error ; Automatically scroll to first
-      )
+(define-key c-mode-map (kbd "M-RET") 'srefactor-refactor-at-point)
+(define-key c++-mode-map (kbd "M-RET") 'srefactor-refactor-at-point)
 
 
 (provide 'ds-cpp)
