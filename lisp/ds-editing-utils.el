a24390da00306a5e3a9eac351abfe2451a5a1766
Dimitar Stanev <jimstanev@gmail.com>
2018-05-17 17:19:36 +0300

    unfill paragraph

--- lisp/ds-editing-utils.el
+++ lisp/ds-editing-utils.el
@@ -143,11 +143,20 @@
              :mode ("\\.fragmentshader\\'" "\\.vertexshader\\'"))
 
 ;; bury warning buffer (may cause problems because all warnings are ignored)
-(use-package unkillable-scratch
-             :ensure t
-             :config
-             (add-to-list 'unkillable-buffers "\\*Warnings\\*$")
-             (setq unkillable-scratch-behavior 'bury))
+;; (use-package unkillable-scratch
+;;              :ensure t
+;;              :config
+;;              (add-to-list 'unkillable-buffers "\\*Warnings\\*$")
+;;              (setq unkillable-scratch-behavior 'bury))
 
+
+;;; Stefan Monnier <foo at acm.org>. It is the opposite of fill-paragraph
+(defun ds/unfill-paragraph (&optional region)
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max))
+        ;; This would override `fill-column' if it's an integer.
+        (emacs-lisp-docstring-fill-column t))
+    (fill-paragraph nil region)))
 
 (provide 'ds-editing-utils)
