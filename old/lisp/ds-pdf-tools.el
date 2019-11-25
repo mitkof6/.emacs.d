8c5144cc38099595c8d946fe7e55c3f74103ced5
Dimitar Stanev <dimitar.stanev@epfl.ch>
2019-11-25 12:43:56 +0100

    clean old code

--- old/lisp/ds-pdf-tools.el
+++
@@ -1,8 +0,0 @@
-(use-package pdf-tools
-  :ensure t
-  :init (pdf-tools-install))
-
-;; (add-to-list 'pdf-tools-enabled-modes 'pdf-view-midnight-minor-mode)
-;; (setq pdf-view-midnight-colors '("#d6d6d6" . "#000000"))
-
-(provide 'ds-pdf-tools)
--- old/lisp/unused/ds-evil.el
+++
@@ -1,4 +0,0 @@
-(require-package 'evil)
-(setq evil-want-C-u-scroll t)
-(require 'evil)
-(provide 'init-evil)
